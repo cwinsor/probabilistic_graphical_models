a77443fcef0267d2ac59bd1c40ef3a1829b851bd
cwinsor <cwinsor@gmail.com>
2016-01-20 11:32:31 -0500

    small change

--- src/cwinsor/us/a/pgm/App04_coursera_pgm_figure_3_4.java
+++ src/cwinsor/us/a/pgm/App04_coursera_pgm_figure_3_4.java
@@ -25,20 +25,6 @@ public class App04_coursera_pgm_figure_3_4 {
 	 */
 	public void run() {
 
-		/* add a mapping function to the construction of a random variable - future */
-		/* a random variable instance, and a distribution instance are essentially the same thing */
-		/* the assignment of 'values' to a random variable or a distribution is replaced by the mapping function */
-		/* a joint distribution is a specific case of a random variable - in the case of a
-		 * joint distribution the overall sum is 1
-		 * a conditional distribution is a specific case of a random variable - in the case of
-		 * a conditional distribution the sum over the condition-ing variables = 1.0
-		 * There is also the third case of a random variable - an 'uncontrolled or unconstrained' type - where it
-		 * does not necessarily sum to anything.
-		 * 
-		 * Question: Definition vs instance:  Does the definition necessarily establish which of the sub-classes ?
-		 * Answer: I would think absolutely yes - the sub-class is established at the time of definition.
-		 */
-
 		// define random variables and events
 		RandomVariableDefinition RVDEF_DIFFICULTY = new RandomVariableDefinition("RVDEF_DIFFICULTY", Arrays.asList("d0", "d1"));
 		RandomVariableDefinition RVDEF_INTELLIGENCE = new RandomVariableDefinition("RVDEF_INTELLIGENCE", Arrays.asList("i0", "i1"));
@@ -46,29 +32,7 @@ public class App04_coursera_pgm_figure_3_4 {
 		RandomVariableDefinition RVDEF_LETTER = new RandomVariableDefinition("RVDEF_LETTER", Arrays.asList("l0", "l1"));
 		RandomVariableDefinition RVDEF_SAT = new RandomVariableDefinition("RVDEF_SAT", Arrays.asList("s0", "s1"));
 
-		// instance random variables
-		/* false - random variable is defined as a function on some distribution ???
-		 * Assuming that is true - the constructor should take in a mapping function as one
-		 * of the parameters.   The other parameter would be (a list of variable-defs??)
-		 * The degenerate case is to enumerate (case switch) the events and return the pre-defined value.
-		 * A number of good examples exist, but would return an Object (type TBD).
-		 */
-		//		RandomVariableInstance difficulty = new RandomVariableInstance("difficulty", RV_DEF_DIFFICULTY);
-		// here we want to set the callback function that defines the behavior
-		//	difficulty.set(difficulty.eventRef("d0"), 0.6);
-		//	difficulty.set(difficulty.eventRef("d0"), 0.4);
-		//		RandomVariableInstance intelligence = new RandomVariableInstance("intelligence", RV_DEF_INTELLIGENCE);
-		//		RandomVariableInstance grade = new RandomVariableInstance("grade", RV_DEF_GRADE);
-
-
-	//	DistributionDefinition DIST_DEF_DIFFICULTY = new DistributionDefinition("DIST_DEF_DIFFICULTY", RV_DEF_DIFFICULTY, Arrays.asList());
-	//	DistributionDefinition DIST_DEF_GRADE = new DistributionDefinition("DIST_DEF_GRADE", RV_DEF_GRADE, Arrays.asList(RV_DEF_DIFFICULTY, RV_DEF_INTELLIGENCE));
-	//	DistributionDefinition DIST_DEF_INTELLIGENCE = new DistributionDefinition("DIST_DEF_INTELLIGENCE", RV_DEF_GRADE, Arrays.asList(RV_DEF_DIFFICULTY, RV_DEF_INTELLIGENCE));
-	//	DistributionDefinition DIST_DEF_LETTER = new DistributionDefinition("DIST_DEF_LETTER", RV_DEF_LETTER, Arrays.asList(RV_DEF_GRADE));
-	//	DistributionDefinition DIST_DEF_SAT = new DistributionDefinition("DIST_DEF_SAT", RV_DEF_SAT, Arrays.asList(RV_DEF_INTELLIGENCE));
-
-
-
+		// instance local probability models	
 		LocalProbabilityModel difficulty = new LocalProbabilityModel("difficulty");
 		difficulty.setTargetAndDependencies(RVDEF_DIFFICULTY, null);
 		difficulty.setValue("d0", 0.6);
